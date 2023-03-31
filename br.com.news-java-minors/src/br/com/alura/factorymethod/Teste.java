9d6ff0c139054d1bf8cc637b44f9b5dbdbb32789
Arian Botine <arian.botine@shift.com.br>
2023-03-30 23:39:30 -0300

    Utilizando o of para criar listas imutavais e reduzir linhas de código

--- br.com.news-java-minors/src/br/com/alura/factorymethod/Teste.java
+++ br.com.news-java-minors/src/br/com/alura/factorymethod/Teste.java
@@ -3,15 +3,22 @@ package br.com.alura.factorymethod;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Teste {
-
 	public static void main(String[] args) {
+		List<String> nomesImutavies = List.of("primeiroNome","segundoNome","terceiroNome");
+		Set<String> nomesPorSet = Set.of("primeiroNome","segundoNome","terceiroNome");
+		Map.of("nome","João");
+		/*
 		ArrayList<String> nomes = new ArrayList<>();
 		nomes.add("primeiroNome");
 		nomes.add("segundoNome");
 		nomes.add("terceiroNome");
 		List<String> nomesImutavel = Collections.unmodifiableList(nomes);
-		System.out.println(nomesImutavel);
+		*/
+		System.out.println(nomesImutavies);
+		System.out.println(nomesPorSet);
 	}
 }
